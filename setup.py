6cb6b2ca6fe3fac63588c34bed8a31bd300260c0
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2023-07-24 10:41:02 -0700

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*names, **kwargs):
 
 setup(
     name='geophires-x',
-    version='1.0.2',
+    version='1.0.3',
     license='MIT',
     description='Distributable python package version of https://github.com/malcolm-dsider/GEOPHIRES-X',
     long_description='{}\n{}'.format(
